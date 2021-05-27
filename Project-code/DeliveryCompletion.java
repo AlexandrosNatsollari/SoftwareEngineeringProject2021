bae46fbb0fb3be376d2265af5c1c9b00588fb9c0
PANAGIOTIS-BARMPOUNIS <81076021+PANAGIOTIS-BARMPOUNIS@users.noreply.github.com>
2021-05-27 16:47:45 +0300

    Update DeliveryCompletion.java

--- Project-code/DeliveryCompletion.java
+++ Project-code/DeliveryCompletion.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.*;
 
 public class DeliveryCompletion 
         extends JFrame
@@ -9,7 +10,8 @@ public class DeliveryCompletion
     private Container c;
     private JLabel title;
     
-    
+    private JLabel DeliveryID;
+    private JTextField tDeliveryID;    
     private JLabel Completion_Time;
     private JTextField tCompletion_Time;
     
@@ -18,6 +20,7 @@ public class DeliveryCompletion
     
     private JButton sub;
     private JButton reset;
+    private JButton close;
     private JTextArea tout;
     private JLabel res;
     private JTextArea resadd;
@@ -43,20 +46,31 @@ public class DeliveryCompletion
         c.add(title);
           
         
+        DeliveryID = new JLabel("Delivery ID");
+        DeliveryID.setFont(new Font("Arial", Font.PLAIN, 20));
+        DeliveryID.setSize(100, 20);
+        DeliveryID.setLocation(120, 100);
+        c.add(DeliveryID);
+  
+        
+        tDeliveryID = new JTextField();
+        tDeliveryID.setFont(new Font("Arial", Font.PLAIN, 15));
+        tDeliveryID.setSize(180, 20);
+        tDeliveryID.setLocation(300, 100);
+        c.add(tDeliveryID);
+
         Completion_Time = new JLabel("Delay Time");
         Completion_Time.setFont(new Font("Arial", Font.PLAIN, 20));
         Completion_Time.setSize(100, 20);
-        Completion_Time.setLocation(120, 200);
+        Completion_Time.setLocation(120, 140);
         c.add(Completion_Time);
   
         
         tCompletion_Time = new JTextField();
         tCompletion_Time.setFont(new Font("Arial", Font.PLAIN, 15));
         tCompletion_Time.setSize(180, 20);
-        tCompletion_Time.setLocation(300, 200);
+        tCompletion_Time.setLocation(300, 140);
         c.add(tCompletion_Time);
-
-  
         ///////////////////////////////////////////////////////////////////
         
         
@@ -68,20 +82,27 @@ public class DeliveryCompletion
         sub = new JButton("Submit");
         sub.setFont(new Font("Arial", Font.PLAIN, 15));
         sub.setSize(100, 20);
-        sub.setLocation(150, 300);
+        sub.setLocation(150, 200);
         sub.addActionListener(this);
         c.add(sub);
   
         reset = new JButton("Reset");
         reset.setFont(new Font("Arial", Font.PLAIN, 15));
         reset.setSize(100, 20);
-        reset.setLocation(340, 300);
+        reset.setLocation(340, 200);
         reset.addActionListener(this);
         c.add(reset);
+        
+        close = new JButton("Close");
+        close.setFont(new Font("Arial", Font.PLAIN, 15));
+        close.setSize(100, 20);
+        close.setLocation(250, 300);
+        close.addActionListener(this);
+        c.add(close);
   
         tout = new JTextArea();
         tout.setFont(new Font("Arial", Font.PLAIN, 15));
-        tout.setSize(300, 200);
+        tout.setSize(300, 180);
         tout.setLocation(550, 100);
         tout.setLineWrap(true);
         tout.setEditable(false);
@@ -110,33 +131,30 @@ public class DeliveryCompletion
             
                 String data1;
                 String data
-                    = "Completion Time: " 
-                      + tCompletion_Time.getText();
-                      
-                
-                      
-                
-  
-                
+                    = "Delivery ID: "+
+                       tDeliveryID.getText() + "\n"+
+                      "Completion Time: " 
+                      + tCompletion_Time.getText();                 
+                               
                 tout.setText(data);
                 tout.setEditable(false);
                 res.setText("Form Submitted Successfully..");
-            
         }
   
         else if (e.getSource() == reset) {
             String def = "";
-                     
+            tDeliveryID.setText(def);        
             tCompletion_Time.setText(def);
             res.setText(def);
             tout.setText(def);    
             resadd.setText(def);
         }
-    }
         
-
-        
+        else if (e.getSource() == close){
+            setVisible(false);
+        }
     }
+  }
 
    class Completion {  
     public static void main(String[] args) throws Exception
