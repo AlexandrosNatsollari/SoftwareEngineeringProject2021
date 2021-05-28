a840d05dc3cb08a83cf344a4957531ff6041ef47
PANAGIOTIS-BARMPOUNIS <81076021+PANAGIOTIS-BARMPOUNIS@users.noreply.github.com>
2021-05-28 14:54:37 +0300

    Update DriverMainScreenButtons.java

--- Project-code/DriverMainScreenButtons.java
+++ Project-code/DriverMainScreenButtons.java
@@ -13,115 +13,102 @@ import javax.swing.border.TitledBorder;
 
 public class DriverMainScreenButtons {
   public static void main(String... args) {
-   final JFrame f = new JFrame();
-   TitledBorder border = new TitledBorder("DriverMainScreen");
-    border.setTitleJustification(TitledBorder.CENTER);
-    border.setTitlePosition(TitledBorder.TOP);
+        final JFrame f = new JFrame();
+        TitledBorder border = new TitledBorder("DriverMainScreen");
+        border.setTitleJustification(TitledBorder.CENTER);
+        border.setTitlePosition(TitledBorder.TOP);
    
         // Create OK button
         JButton btn1 = new JButton("Show Map");
    
         // Add event handler for OK button
         btn1.addActionListener(
-                new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(f,
-                                "Map"
-                                );
+        new ActionListener()
+        {
+              public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(f,"Map");
                     }
-                });
+                }
+        );
         // Create Cancel button
-        JButton btn2 = new JButton("Travel Expenses");
+        JButton btn2 = new JButton("Expenses Form");
         btn2.addActionListener(
-                new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(f,
-                                "Form"
-                                );
+        new ActionListener()
+        {
+             public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==btn2)
+                            {
+                                Expenses_Form c = new Expenses_Form();                                                              
+                            }
                     }
-                });
+                }
+        );
                 
                 
-                JButton btn3 = new JButton("Update Order");
+        JButton btn3 = new JButton("Delivery Update");
         btn3.addActionListener(
-                new ActionListener(){
+         new ActionListener()
+         {
                     public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(f,
-                                "Update"
-                                );
+                        if(e.getSource()==btn3)
+                            {
+                                DeliveryUpdate f = new DeliveryUpdate();                                                              
+                            }
                     }
-                });
+                }
+        );
                 
                 
-                      JButton btn4 = new JButton("Notifications");
+        JButton btn4 = new JButton("Notifications");
         btn4.addActionListener(
-                new ActionListener(){
+         new ActionListener()
+         {
                     public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(f,
-                                "Showed Notification"
-                                );
+                        JOptionPane.showMessageDialog(f,"Showed Notification");
                     }
-                });
+                }
+        );
                 
-                                JButton btn5 = new JButton("Emergency");
+        JButton btn5 = new JButton("Emergency");
         btn5.addActionListener(
-                new ActionListener(){
+         new ActionListener()
+         {
                     public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(f,
-                                "Emergency"
-                                );
+                        if(e.getSource()==btn5)
+                            {
+                                EmergencyButtons c = new EmergencyButtons();                                                              
+                            }
                     }
-                });
+                }
+        );
 
 
-    JPanel p = new JPanel();
-     p.setBorder(border);
-    f.add(p);
+        JPanel p = new JPanel();
+         p.setBorder(border);
+        f.add(p);
  
-    f.setVisible(true);     
+        f.setVisible(true);     
      
      
      
-    p.setLayout(null);
-    btn1.setBounds(30, 20, 180, 70);
-    btn2.setBounds(30, 100, 180, 70);
-    btn3.setBounds(30, 180, 180, 70);
-    btn4.setBounds(30, 260, 180, 70);
-    btn5.setBounds(30, 350, 180, 70);
+        p.setLayout(null);
+        btn1.setBounds(30, 20, 180, 70);
+        btn2.setBounds(30, 100, 180, 70);
+        btn3.setBounds(30, 180, 180, 70);
+        btn4.setBounds(30, 260, 180, 70);
+        btn5.setBounds(30, 350, 180, 70);
     
-    p.add(btn1);
-    p.add(btn2);
-    p.add(btn3);
-    p.add(btn4);
-    p.add(btn5);
+        p.add(btn1);
+        p.add(btn2);
+        p.add(btn3);
+        p.add(btn4);
+        p.add(btn5);
 
 
-    f.add(p);
-    // setLayout(null);
-    f.setDefaultCloseOperation(3);
-    f.setSize(1000, 1000);
-    f.setVisible(true);
+        f.add(p);
+        // setLayout(null);
+        f.setDefaultCloseOperation(3);
+        f.setSize(1000, 1000);
+        f.setVisible(true);
+      }
   }
-  
-  
-  
-  
-  
-  
-  
-  
- 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-}
