b4ad05fdff1d481cd49ab1673b0da5a72524be69
PANAGIOTIS-BARMPOUNIS <81076021+PANAGIOTIS-BARMPOUNIS@users.noreply.github.com>
2021-06-08 13:51:54 +0300

    Update DeliveryUpdate.java

--- Project-code/DeliveryUpdate.java
+++ Project-code/DeliveryUpdate.java
@@ -4,10 +4,7 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
  
 public class DeliveryUpdate{
